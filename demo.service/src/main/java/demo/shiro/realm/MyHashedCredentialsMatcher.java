99aa80efa37048d744450e9b326d1c5d3dcc30d8
zhengjianhui <815168733@qq.com>
2017-02-07 17:00:00 +0800

    增加密码错误提示信息

--- demo.service/src/main/java/demo/shiro/realm/MyHashedCredentialsMatcher.java
+++ demo.service/src/main/java/demo/shiro/realm/MyHashedCredentialsMatcher.java
@@ -1,5 +1,6 @@
 package demo.shiro.realm;
 
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
@@ -11,8 +12,18 @@ public class MyHashedCredentialsMatcher extends HashedCredentialsMatcher {
 
     @Override
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
-        return super.doCredentialsMatch(token, info);
+        boolean flag = super.doCredentialsMatch(token, info);
+
+        if(!flag) {
+            throw new AuthenticationException("密码错误");
+        }
+
+        return flag;
 
         // TODO 此处可配合 缓存 做登入次数验证
     }
+
+
+
+
 }
